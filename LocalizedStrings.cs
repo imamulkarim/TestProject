cfab9f5236d0ea63bdfe061569a5f991ac1f2ffd
imamulkarim <imamul.karim@gmail.com>
2014-02-22 19:07:31 +0600

    test

---
+++ LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using TestProject.Resources;
+
+namespace TestProject
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
